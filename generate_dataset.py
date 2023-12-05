2512e003ef1a5d49dc7648149f44cbc9a144369b
Oleh Basystyi <basystyo@gmail.com>
2023-12-05 12:09:17 +0200

    removed test stuff; restructured the project

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,9 +1,14 @@
-import random 
+"""
+Generates random data sets for the problem
+"""
+import random
 
-ROWS = 1000
-COLS = 1000
+ROWS = 5000
+COLS = 5000
 MAX_HEIGHT = 10000
 
-with open('data.txt', 'w') as data_file:
+with open('data.txt', 'w', encoding='utf-8') as data_file:
     for i in range(0, ROWS):
-        data_file.write(' '.join( [ str(random.randint(1, MAX_HEIGHT)) for i in range(COLS) ] ) + '\n')
+        data_file.write(' '.join( [ str(random.randint(1, MAX_HEIGHT))\
+                                    for _ in range(COLS) ] ) + '\n')
+print("DATA GENERATION FINISHED")
--- lab.py
+++ lab.py
@@ -1,23 +1,41 @@
 """
-lab2
+Shortest path between 2 points on the surface problem
+member1 = <Zhuk Maksym>
+member2 = <Basystyi Oleh>
+member3 = <Rudish Artur>
+member4 = <Popeniuk Sofia>
+member5 = <Marych Maksym>
+GitHub repo: github.com/n1n1n1q/Shortest-Path-On-Surface
 """
-import cProfile
 import heapq
 
-
 class PriorityQueue:
+    """
+    Implemetation of priority queue class.
+    """
     def __init__(self):
+        """
+        Queue's initialization
+        """
         self.elements = []
 
     def empty(self):
+        """
+        Empties the queue
+        """
         return len(self.elements) == 0
 
     def put(self, item, priority):
+        """
+        Puts the element into the queue
+        """
         heapq.heappush(self.elements, (priority, item))
 
     def get(self):
+        """
+        Gets queue's last element
+        """
         return heapq.heappop(self.elements)[1]
-
 
 def read_file(filename: str)-> list[list]:
     """
@@ -34,30 +52,20 @@ def read_file(filename: str)-> list[list]:
                       for line in file.readlines()  ]
     return raw_data
 
-DISTANCE_CACHE = {}
-
 def get_distance(start_point: (int, int), 
                         end_point: (int, int),
                         height_matrx: list[list], 
                         step: int)-> list[list]:
     """
-    >>> get_distance((0, 0), (1, 2), [[1, 14, 15, 234], [23, 34, 5, 5]], 2)
-    inf
+    Finds distance between 2 neighbour points
+    >>> get_distance((0, 0), (0, 1), [[1, 14, 15, 234], [23, 34, 5, 5]], 1)
+    13.038404810405298
     >>> get_distance((0, 0), (0, 1), [[5,2],[1,2]],4)
     5.0
     """
-    #if (start_point, end_point) in DISTANCE_CACHE:
-     #   return DISTANCE_CACHE[(start_point, end_point)]
-
-    distance = height_matrx[start_point[0]][start_point[1]] - height_matrx[end_point[0]][end_point[1]]
+    distance = height_matrx[start_point[0]][start_point[1]]-height_matrx[end_point[0]][end_point[1]]
     distance = (step ** 2 + distance ** 2) ** 0.5
     return distance
-    #DISTANCE_CACHE[(start_point, end_point)] = distance
-    #return DISTANCE_CACHE[(start_point, end_point)]
-
-
-NEIGHBOURS_CACHE = {}
-
 
 def get_neighbours(point: (int, int),matrix: list[list])-> list[tuple]:
     """
@@ -65,10 +73,6 @@ def get_neighbours(point: (int, int),matrix: list[list])-> list[tuple]:
     >>> get_neighbours((1,1),[[1,1,1,1],[1,1,1,1]])
     [(1, 2), (0, 1), (1, 0)]
     """
-
-    #if point in NEIGHBOURS_CACHE:
-     #   return NEIGHBOURS_CACHE[point]
-
     neighbours=[]
     coordinates_delta=[1,-1]
     for i in range(2):
@@ -76,7 +80,6 @@ def get_neighbours(point: (int, int),matrix: list[list])-> list[tuple]:
             neighbours.append((point[0]+coordinates_delta[i], point[1]))
         if 0<=point[1]+coordinates_delta[i] < len(matrix[0]):
             neighbours.append((point[0],point[1]+coordinates_delta[i]))
-    #NEIGHBOURS_CACHE[point] = neighbours    
     return neighbours
 
 def is_valid(point: (int,int), matrix: list[list])-> bool:
@@ -85,8 +88,7 @@ def is_valid(point: (int,int), matrix: list[list])-> bool:
     >>> is_valid((10,10),[[1,1],[2,3]])
     False
     """
-    return point[0] in range(0,len(matrix)) and point[1] in range(0,len(matrix[0]))
-
+    return 0<=point[0]<len(matrix) and 0<=point[1]<len(matrix[0])
 
 def heuristic(start_point: (int, int), end_point: (int, int)) -> int:
     """
@@ -95,7 +97,6 @@ def heuristic(start_point: (int, int), end_point: (int, int)) -> int:
     12
     """
     return abs(start_point[0] - end_point[0] ) + abs(start_point[1] - end_point[1])
-
 
 def reconstruct_path(came_from: dict, current: (int, int)):
     """
@@ -108,14 +109,14 @@ def reconstruct_path(came_from: dict, current: (int, int)):
             total_path.insert(0, current)
     return total_path
 
-
 def a_star(start_point: (int,int),
            end_point: (int,int),
            height_matrix: list[list[int]],
            step: int)-> float:
     """
     A star algorithms
-
+    >>> a_star((0,0), (2,1), [[1,1,1,1],[1000,1000,100,1],[1,1,1,1]],1)
+    [(0, 0), (0, 1), (0, 2), (0, 3), (1, 3), (2, 3), (2, 2), (2, 1)]
     """
     if not is_valid(start_point,height_matrix) or not is_valid(end_point,height_matrix):
         return -1
@@ -129,7 +130,6 @@ def a_star(start_point: (int,int),
 
     while not open_set.empty():
         current = open_set.get()
-
         if current == end_point:
             return reconstruct_path(came_from, current)
         for next_point in get_neighbours(current, height_matrix):
@@ -145,53 +145,19 @@ def a_star(start_point: (int,int),
 
     return -1
 
-t=read_file('data.txt')
-print("file_read finished")
-
-start_point = (0, 0)
-end_point = (4999,4999)
-
-import time
-start = time.time()
-result = a_star(start_point=start_point, end_point=end_point, height_matrix=t, step=1)
-print(time.time() - start)
-
-#print(result)
-
-
-
-
-
-
-
-#print(a_star((0,0),(4,4),read_file('data.txt'),1))
-# if __name__=='__main__':
-    # import doctest
-    # print(doctest.testmod())
-    # import time
-    # from random import randint
-    # height_matrix = read_file("data.txt")
-    
-    # def print_rand_elem(matrix):
-    #     print(matrix[0][1])
-    
-    # func_total=0
-    # no_func_total=0
-    # for i in range(250000000):
-    #     t=time.time()
-    #     print_rand_elem(height_matrix)
-    #     t=time.time()-t
-    #     func_total+=t
-    #     print(f"func time = {t}")
-    #     t=time.time()
-    #     print(height_matrix[0][1])
-    #     t=time.time()-t
-    #     no_func_total+=t
-    #     print(f"no func time ={t} ")
-    # print(f"{func_total=},{no_func_total}")
-    # print(f"readfile time = {time.time()-t}")
-    # t=time.time()
-    # start = (0, 0)
-    # end = (3000, 3000)
-    # a_star(start_point=start, end_point=end, height_matrix=height_matrix, step=1)
-    # print(f"a_star time  = {time.time()-t}")
+if __name__=='__main__':
+    import doctest
+    print(f'DOCTETS\
+\n==================================\
+            \n{doctest.testmod()}\
+\n==================================\n')
+    import time
+    start_time=time.time()
+    data=read_file('data.txt')
+    print(f"Reading file finished; ~ time = {time.time()-start_time}")
+    start_p,end_p,st=(0,0),(2,1),1
+    print(f"Start point = {start_p}, end point = {end_p}, step = {st}")
+    astar_start_time=time.time()
+    a_star_output=a_star(start_point=start_p,end_point=end_p,height_matrix=data,step=st)
+    print(f"Finished calculatin distance; ~ time = {time.time()-astar_start_time}")
+    print(f"Shortest path = {a_star_output}\nTotal time: {time.time()-start_time}")
